93bd13a36dd155e513da91a69b6657f2f82e57d4
Joan Andrés <joanandres318@gmail.com>
2022-01-22 21:54:44 -0500

    Se agrega función de ingreso de nuúmero maximo a imprimir

--- consoleNet/FizzBuzz.cs
+++ consoleNet/FizzBuzz.cs
@@ -2,23 +2,40 @@
 
 
 public class fizzBuzz
-{   
+{
     //Se declara el arreglo type int[] y se inicializa con 15 valores
-    int[] listNum = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    public int[] Valores(int num)
+    {
+        int[] listNum = new int[num];
+        for (int i = 1; i < num; i++)
+        {
+            listNum[i - 1] = i;
+        }
+        return listNum;
+    }
+
 
     static void Main(string[] args)
     {
-        fizzBuzz lista = new fizzBuzz();
-        int[] list = lista.listNum;
-        for (int i = 0; i < list.Length; i++)
+        try
         {
-            if (list[i] % 3 == 0 && list[i] % 5 ==  0) Console.WriteLine("FizzBuzz");
-            else if (list[i] % 3 == 0) Console.WriteLine("Fizz");
-            else if (list[i] % 5 == 0) Console.WriteLine("Buzz");
-            else Console.WriteLine(list[i]);
-            
+            fizzBuzz lista = new fizzBuzz();
+            Console.Write("Please enter a namber: ");
+            var value = Convert.ToInt32(Console.ReadLine());
+            int[] list = lista.Valores(value);
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] % 3 == 0 && list[i] % 5 == 0) Console.WriteLine("FizzBuzz");
+                else if (list[i] % 3 == 0) Console.WriteLine("Fizz");
+                else if (list[i] % 5 == 0) Console.WriteLine("Buzz");
+                else Console.WriteLine(list[i]);
+            }
         }
-        
+        catch (System.Exception e)
+        {
+            Console.WriteLine("Error: "+ e);
+        }
     }
 }
 
