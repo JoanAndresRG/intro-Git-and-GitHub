bd87d19954b0ac4a13d7973e23fe29e3ca4c6723
Joan Andrés <joanandres318@gmail.com>
2022-01-27 20:56:34 -0500

    Se dio solucion al reto mayorMenor de haker rank

--- consoleNet/mayorMenor.cs
+++ consoleNet/mayorMenor.cs
@@ -1,51 +1,44 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-// /*Dados cinco enteros positivos, encuentre los valores mínimo y máximo que se pueden calcular sumando exactamente cuatro de los cinco enteros. Luego imprima los respectivos valores mínimo y máximo como una sola línea de dos enteros largos separados por espacios.
-// **/
+/*Dados cinco enteros positivos, encuentre los valores mínimo y máximo que se pueden calcular sumando exactamente cuatro de los cinco enteros. Luego imprima los respectivos valores mínimo y máximo como una sola línea de dos enteros largos separados por espacios.
+**/
 
-// namespace consoleNet
-// {
-//     public class mayorMenor
-//     {
-//          /*
-//      * Complete the 'miniMaxSum' function below.
-//      *
-//      * The function accepts INTEGER_ARRAY arr as parameter.
-//      */
+namespace consoleNet
+{
+    class Program
+    {
+        public static void miniMaxSum(List<int> arr)
+        {
+            long numMax = 0;
+            long numMin = 0;
+            int maxList = arr.Max();
+            int minList = arr.Min();
+            int indMax = arr.IndexOf(maxList);
+            int indMin = arr.IndexOf(minList);
+            Console.WriteLine($"MaxIten: {maxList}\nMinIten: {minList}");
+            Console.WriteLine($"MaxIdex: {indMax}\nMinIndex: {indMin}");
+            // if (indMin == 0) indMin -= 1;
+            // if (indMax == 1) indMax -= 1;
+            Console.WriteLine("verf min:" + arr[indMin]);
+            arr.Remove(arr[indMin]);
+            List<int> nlMax = new List<int>();
+            foreach (int iten in arr)
+            {
+                nlMax.Add(iten);
+                numMax += iten;
+                Console.WriteLine(iten);
+            }
+            // numMax = Convert.ToInt64(nlMax.Sum());
+            numMin = Convert.ToInt64(numMax - maxList + minList);
+            Console.WriteLine(numMin + " " + numMax);
+        }
 
-//     public static void miniMaxSum(List<int> arr)
-//     {
-//         List<int> newlist = new List<int>();
-//         foreach (int i in arr) {
-//            newlist.Add(i);
-//         }
-//         int max1 = newlist.Max();
-//         int min1 = newlist.Min();
-//         newlist.Reverse();
-//         int cont = 0;
-//         for (int i = 0; i < newlist.Count; ++i)   
-//         {
-//             if (newlist[i] == min1)   
-//             {
-//                 Console.WriteLine(min1);
-//                 newlist.RemoveAt(cont);
-//                 newlist.Sort();
-//                 int res1 = newlist.Sum();
-//                 Console.Write(res1+" ");
-//             }
-//             if (newlist[i] == max1) 
-//             {
-//                 // Console.WriteLine(max1);
-//                 newlist.RemoveAt(cont);
-//                 newlist.Sort();    
-//                 int res2 = newlist.Sum();
-//                 Console.WriteLine(res2+" ");
-//             }
-//             cont ++;
-//         }
-//         Console.WriteLine(cont);
-//     }
-//     }
-// }
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 769082435, 210437958, 673982045, 375809214, 380564127 };
+            miniMaxSum(list);
+        }
+    }
+}
